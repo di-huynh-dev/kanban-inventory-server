74b0a3ff435ee37e07e61b0d9183c192efca770f
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-09-08 12:54:51 +0700

    Set up environment

---
+++ server.ts
@@ -0,0 +1,14 @@
+const serverApp = require("./src/app");
+const {
+  app: { port },
+} = require("./src/config/environment");
+
+const server = serverApp.listen(port, () => {
+  console.log("Server started on port " + port);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Server closed");
+  });
+});
